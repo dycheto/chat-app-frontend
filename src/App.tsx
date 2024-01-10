d59d03c7491abd81a5f187d3cdb05a72528abaca
Tihomir Dukov <t.i.dukov@gmail.com>
2024-01-10 11:40:59 +0200

    Login component done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Navigate replace to="/login"/>}/>
+        <Route path='/login' element={<Login/>} />
+      </Routes>
         
     </div>
   );
---
+++ src/components/Login.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log("Login", username, password);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <span>If you dont have an account <Link to="/register">Register</Link></span>
+      
+    </div>
+  );
+}
+
+export default Login;
